5aff1e794e9584bb166e5983cdbbffa25dfa7caf
Felix23333 <67811692+Felix23333@users.noreply.github.com>
2021-06-08 09:51:28 +0800

    fix end background bug

--- src/scenes/End.js
+++ src/scenes/End.js
@@ -6,13 +6,20 @@ class End extends Phaser.Scene {
 
     preload()
     {
-        this.load.image("endBackground", "assets/EndBackground.jpg");
+        this.load.spritesheet("endBackground", "assets/EndBackgroundNew.jpg", {frameWidth: 960, frameHeight: 720, startFrame: 0, endFrame: 7});
     }
 
     create()
     {
-        this.endBackground = new Background(this, 0, 0, 960, 720, "endBackground", 0, false, true);
-
+        this.endBackground = new VFX(this, 0, 0, "endBackground", 0, false, true);
+        //animation config
+        this.anims.create({
+            key: 'endAnimate',
+            frames: this.anims.generateFrameNames('endBackground', {start: 0, end: 7}),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.endBackground.anims.play('endAnimate', true);
         //Handle scene transition
         menuKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.M);
         creditsKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C);
